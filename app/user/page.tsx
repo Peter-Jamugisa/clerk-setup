34d08a00a0e5d98bbcdb3f83d4a1aedbe3026f95
Peter-Jamugisa <105854604+Peter-Jamugisa@users.noreply.github.com>
2024-07-22 08:08:39 +0300

    Add page.tsx

---
+++ app/user/page.tsx
@@ -0,0 +1,12 @@
+import UserDashboard from "@/components/userdashboard";
+
+
+const UserPage: React.FC = () => {
+  return (
+    <div>
+      <UserDashboard />
+    </div>
+  );
+};
+
+export default UserPage;
