6970b791a5ef7cb22473c3c191dce54ee4fa9e26
Peter-Jamugisa <105854604+Peter-Jamugisa@users.noreply.github.com>
2024-08-01 19:36:03 +0300

    Add page.tsx

---
+++ app/recordings/page.tsx
@@ -0,0 +1,16 @@
+import Records from "@/components/record";
+import NavBar from "@/components/ui/Navbar";
+import React from "react";
+
+function Recordings() {
+  return (
+    <div>
+      <NavBar />
+      <div>
+        <Records />
+      </div>
+    </div>
+  );
+}
+
+export default Recordings;
